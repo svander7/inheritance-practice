727c9ddbced4872d703fa9e94ead3f4cf268346c
Sonny Vanderhoeven <147276430+svander7@users.noreply.github.com>
2023-12-19 09:52:45 -0500

    Initial run and completed

--- src/com/coderscampus/base/NormalUser.java
+++ src/com/coderscampus/base/NormalUser.java
@@ -2,4 +2,9 @@ package com.coderscampus.base;
 
 public class NormalUser extends User {
 
+	public NormalUser() {
+        this.setReadAccess(true);
+        this.setWriteAccess(true);
+        this.setSuperUser(false);
+    }
 }
--- src/com/coderscampus/base/SuperUser.java
+++ src/com/coderscampus/base/SuperUser.java
@@ -1,4 +1,10 @@
 package com.coderscampus.base;
 
 public class SuperUser extends User {
+	
+	public SuperUser() {
+        this.setReadAccess(true);
+        this.setWriteAccess(true);
+        this.setSuperUser(true);
+    }
 }
